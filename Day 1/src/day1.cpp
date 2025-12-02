684ad55bfa576f702174618d39aa2894595a138d
Nicholas Cieplensky <cieplenskynicholas@gmail.com>
2025-12-02 01:08:13 -0500

    First test commit

---
+++ Day 1/src/day1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by nick on 12/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!\n" << argc << " , " << argv[1] << std::endl;
+
+    return 0;
+}
